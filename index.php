8069e69af29144d0c8fc9f6e5f663dd8b605260a
mharoot <michaelharootoonyan@yahoo.com>
2018-08-26 15:58:47 -0700

    testing

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <title>Spring Boot Quickstart</title>
   <meta charset="utf-8">
@@ -7,79 +8,12 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<!-- <h1 id="springbootquickstart">Spring Boot Quickstart</h1>
-
-
-
-
-<h3 id="unit2springmvctheviewtier">Unit 2 Spring MVC: The View Tier</h3>
-
-<ul>
-<li><a href="#How-Spring-MVC-Works">How Spring MVC Works</a> </li>
-
-<li><a href="#The-REST-API-we'll-build">The REST API we'll build</a></li>
-
-<li><a href="#Creating-a-business-service">Creating a business service</a></li>
-
-<li><a href="#Getting-a-single-resource">Getting a single resource</a> </li>
-
-<li><a href="#Creating-a-new-resource-using-POST">Creating a new resource using POST</a></li>
-
-<li><a href="#Implementing-Update-and-Delete">Implementing Update and Delete</a></li>
-</ul>
-
-<h3 id="unit3bootingspringboot">Unit 3 Booting Spring Boot</h3>
-
-<ul>
-<li><a href="#Unit-Overview">Unit Overview</a></li>
-
-<li><a href="#Using-Spring-Initializr">Using Spring Initializr</a></li>
-
-<li><a href="#Using-Spring-Boot-CLI">Using Spring Boot CLI</a></li>
-
-<li><a href="#Using-the-STS-IDE">Using the STS IDE</a></li>
-
-<li><a href="#Using-application-properties">Using application properties</a></li>
-</ul>
-
-<h3 id="unit4springdatajpathedatatier">Unit 4 Spring Data JPA: The Data Tier</h3>
-
-<ul>
-<li><a href="#What-is-JPA?">What is JPA?</a></li>
-
-<li><a href="#Adding-Spring-Data-JPA">Adding Spring Data JPA</a></li>
-
-<li><a href="#Creating-a-Spring-Data-JPA-Repository">Creating a Spring Data JPA Repository</a></li>
-
-<li><a href="#Making-Crud-Operations-with-Repository">Making Crud Operations with Repository</a></li>
-
-<li><a href="#Adding-Course-APIs">Adding Course APIs</a></li>
-
-<li><a href="#Adding-Entity-Relationship-and-Extending-Repository">Adding Entity Relationship and Extending Repository</a></li>
-</ul>
-
-<h3 id="unit5deploymentandmonitoring">Unit 5 Deployment and monitoring</h3>
-
-<ul>
-<li><a href="#Packaging-and-running-a-Spring-Boot-app">Packaging and running a Spring Boot app</a></li>
-
-<li><a href="#Spring-Boot-Actuator">Spring Boot Actuator</a></li>
-</ul>
-
-<hr />
-
-<h2 id="unit1introduction-1">Unit 1 Introduction</h2>
-
-<h5 id="settingupdevelopmentenvironmentsettingupdevelopmentenvironment"><a href="#Setting-Up-Development-Environment">Setting Up Development Environment</a></h5>
- -->
-
-
-<body data-gr-c-s-loaded="true">
+<body>
 <nav id="navbar" class="container-fluid navbar navbar-toggleable-md navbar-light bg-faded">
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href=".">Spring Boot QuickStart</a>
+  <a class="navbar-brand" href=".">Elegant</a>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -88,13 +22,12 @@
     </ul>
   </div>
 </nav>
-<br>
-<br>
-<br>
-<br>
+</br>
+</br>
+</br>
+</br>
 <div style="background-color:#fff" class="jumbotron">
 <article>
-<h1>Spring Boot &amp; Java: Getting Started</h1>
 <ul>
     <li>
         <a href="#unit1">Unit 1 Introduction</a>
@@ -112,9 +45,11 @@
 
 
 
-<!-- #Setting-Up-Development-Environment -->
-<p><a name="Setting-Up-Development-Environment"></a></p>
-<h2><a href="#Setting-Up-Development-Environment">Setting Up Development Environment</a></h2>
+
+
+<!-- inserting-and-updating-models -->
+<p><a name="inserting-and-updating-models"></a></p>
+<h2><a href="#inserting-and-updating-models">Inserting &amp; Updating Models</a></h2>
 
 
 
@@ -124,15 +59,14 @@
 
 
 
-</article></div>
+</div>
+</article>
+</div>
 
-
-
-
+</body>
 <footer class="footer">
       <div class="container-fluid">
-        <span class="text-muted"> <p style="color:white; font:bold;" class="text-center">©SpringBootQuickStart</p> </span>
+        <span class="text-muted"> <p style="color:white; font:bold;" class="text-center">©Michael Harootoonyan</p> </span>
       </div>
 </footer>
-</body>
 </html>
